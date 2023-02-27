08511caeb538b78fcf9a469c9e5d7bd2f521e96f
Elibrer <eli.brer@gmail.com>
2023-02-27 23:45:57 +1100

    Final password string is now shuffled so initial character types aren't in the same order

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,21 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var includeNumbers = prompt;
 
-
-
-// Write password to the #password input
-
 function writePassword() {
-
   // var password = generatePassword();
+
+
+  //Assign passwordText to the html textbox & password declaration
   var passwordText = document.querySelector("#password");
   var password = "";
+  var passwordArr = [];
 
   //Char length 
   var lengthCorrect = true;
   var passwordLength = 8;
 
-  //Char types
+  //Char types loop
   var charTypes = false;
   
   var uppercase = false;
@@ -24,28 +23,32 @@ function writePassword() {
   var numeric = false;
   var special = false;
 
+  //Char type declarations
   var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var numericChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
   var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*"];
-  
+
+  //All chars declaration
+  var allChars = []; 
+
+  //Var for ensuring one of each char type is included in final password
+  var guaranteedCharsNumber = 0;
   
   //Char length loop
   while (lengthCorrect === true) {
     var passwordLength = prompt("How many characters would you like? (min 8, max 128)", passwordLength);
     console.log("Password Length: " + passwordLength);
     
-
-
-    if (passwordLength <= 7 || passwordLength >= 129) {
+    if (passwordLength === null) {
+      alert("Generate tool terminated.");
+      return;
+    } else if (passwordLength <= 7 || passwordLength >= 129) {
       alert("Password length needs to be more than 8 characters, or less than 128 characters. Please try again.");
       lengthCorrect = true;
     } else if (passwordLength >= 8 && passwordLength <= 128) {
       alert("Password length set to: " + passwordLength);
       lengthCorrect = false;
-    } else if (passwordLength === null) {
-      alert("Generate tool terminated.");
-      return;
     } else {
       alert("Please enter a valid number.");
     }
@@ -59,8 +62,10 @@ function writePassword() {
 
     while (charLoop === true) {
       var promptYN = prompt("Would you like to include uppercase letters? Y/N");
-
-     
+      if (promptYN === null) {
+        alert("Generate tool terminated.");
+        return;
+      }
 
       promptYN = promptYN.toUpperCase();
 
@@ -72,9 +77,6 @@ function writePassword() {
         alert("Uppercase letters will not be included.");
         uppercase = false;
         charLoop = false;
-      } else if (promptYN === null) {
-        alert("Generate tool terminated.");
-        return;
       } else {
         alert("Please enter either Y or N.")
         charLoop = true;
@@ -85,6 +87,10 @@ function writePassword() {
 
     while (charLoop === true) {
       var promptYN = prompt("Would you like to include lowercase letters? Y/N");
+      if (promptYN === null) {
+        alert("Generate tool terminated.");
+        return;
+      }
 
       promptYN = promptYN.toUpperCase();
 
@@ -96,9 +102,6 @@ function writePassword() {
         alert("Lowercase letters will not be included.");
         lowercase = false;
         charLoop = false;
-      } else if (promptYN === null) {
-        alert("Generate tool terminated.");
-        return;
       } else {
         alert("Please enter either Y or N.")
         charLoop = true;
@@ -109,6 +112,10 @@ function writePassword() {
 
     while (charLoop === true) {
       var promptYN = prompt("Would you like to include numeric characters? Y/N");
+      if (promptYN === null) {
+        alert("Generate tool terminated.");
+        return;
+      }
 
       promptYN = promptYN.toUpperCase();
 
@@ -120,9 +127,6 @@ function writePassword() {
         alert("Numeric characters will not be included.");
         numeric = false;
         charLoop = false;
-      } else if (promptYN === null) {
-        alert("Generate tool terminated.");
-        return;
       } else {
         alert("Please enter either Y or N.")
         charLoop = true;
@@ -133,6 +137,10 @@ function writePassword() {
 
     while (charLoop === true) {
       var promptYN = prompt("Would you like to include special characters? Y/N");
+      if (promptYN === null) {
+        alert("Generate tool terminated.");
+        return;
+      }
 
       promptYN = promptYN.toUpperCase();
 
@@ -144,9 +152,6 @@ function writePassword() {
         alert("Special characters will not be included.");
         special = false;
         charLoop = false;
-      } else if (promptYN === null) {
-        alert("Generate tool terminated.");
-        return;
       } else {
         alert("Please enter either Y or N.")
         charLoop = true;
@@ -162,49 +167,56 @@ function writePassword() {
     }
   }
 
-  var allCharsOther = []; 
-  var guaranteedCharsNumber = 0;
-
   // Password generation 
 
   if (lowercase === true) {
     var randomLower = lowercaseChars[Math.floor(Math.random() * lowercaseChars.length)];
-    console.log(randomLower);
-    password += randomLower;
+    console.log("Chosen random lowercase letter: " + randomLower);
+    passwordArr.push(randomLower);
     guaranteedCharsNumber++;
-    allCharsOther = allCharsOther.concat(lowercaseChars);
+    allChars = allChars.concat(lowercaseChars);
   }
 
   if (uppercase === true) {
     var randomUpper = uppercaseChars[Math.floor(Math.random() * uppercaseChars.length)];
-    console.log(randomUpper);
-    password += randomUpper;
+    console.log("Chosen random uppercase letter: " + randomUpper);
+    passwordArr.push(randomUpper);
     guaranteedCharsNumber++;
-    allCharsOther = allCharsOther.concat(uppercaseChars);  
+    allChars = allChars.concat(uppercaseChars);  
   }
 
   if (numeric === true) {
     var randomNumeric = numericChars[Math.floor(Math.random() * numericChars.length)];
-    console.log(randomNumeric);
-    password += randomNumeric;
+    console.log("Chosen random number: " + randomNumeric);
+    passwordArr.push(randomNumeric);
     guaranteedCharsNumber++;
-    allCharsOther = allCharsOther.concat(numericChars);
+    allChars = allChars.concat(numericChars);
   }
 
   if (special === true) {
     var randomSpecial = specialChars[Math.floor(Math.random() * specialChars.length)];
-    console.log(randomSpecial);
-    password += randomSpecial;
+    console.log("Chosen random special character: " + randomSpecial);
+    passwordArr.push(randomSpecial);
     guaranteedCharsNumber++;
-    allCharsOther = allCharsOther.concat(specialChars);
+    allChars = allChars.concat(specialChars);
   }
 
-  console.log("List of all chosen characters: " + allCharsOther);
+  console.log("List of all chosen characters: " + allChars);
 
+  //Adding more characters until passwordLength is satisfied
   for (var i = 0; i < passwordLength - guaranteedCharsNumber; i++) {
-    
-    password += allCharsOther[(Math.floor(Math.random() * allCharsOther.length))];
+    passwordArr.push(allChars[Math.floor(Math.random() * allChars.length)]);
   }
+
+  //Shuffles passwordArr elements
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  shuffle(passwordArr);
+
+  //Converts shuffled array into a string, while adding it to the final password variable
+  password += passwordArr.join("");
 
   document.getElementById("password").value = password;
   passwordText = password;
