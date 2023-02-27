4a7258c9c879fe194d22580bbdab7e3c6f0e6e81
Elibrer <eli.brer@gmail.com>
2023-02-27 13:33:51 +1100

    Various testing to condense code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
-
 var includeNumbers = prompt;
 
 
 
 // Write password to the #password input
 function writePassword() {
+
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
   var password = "";
+
   //Char length 
   var lengthCorrect = false;
   var passwordLength = 8;
@@ -23,7 +23,18 @@ function writePassword() {
   var numeric = false;
   var special = false;
 
+  // var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  // var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  // var numberChars = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  // var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  
+
+  // var chars = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "1234567890",  "!@#$%^&*"];
+  var chars = "";
+  
   var charString = "";
+
+
 
   //Char length loop
   while (lengthCorrect === false) {
@@ -90,54 +101,80 @@ function writePassword() {
     }
   }
 
+
+
   //Password generation 
 
-  if (uppercase === false && lowercase === true && numeric === false && special === false) {
-    charString = "abcdefghijklmnopqrstuvwxyz";
-  } else if (uppercase === true && lowercase === true && numeric === false && special === false) {
-    charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if (uppercase === true && lowercase === true && numeric === true && special === false) {
-    charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  } else if (uppercase === true && lowercase === true && numeric === false && special === true) {
-    charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
-  } else if (uppercase === false && lowercase === true && numeric === true && special === false) {
-    charString = "abcdefghijklmnopqrstuvwxyz1234567890";
-  } else if (uppercase === false && lowercase === true && numeric === false && special === true) {
-    charString = "abcdefghijklmnopqrstuvwxyz!@#$%^&*";
-  } else if (uppercase === false && lowercase === true && numeric === true && special === true) {
-    charString = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
-  } else if (uppercase === true && lowercase === false && numeric === false && special === false) {
-    charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if (uppercase === true && lowercase === false && numeric === true && special === false) {
-    charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-  } else if (uppercase === true && lowercase === false && numeric === true && special === true) {
-    charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
-  } else if (uppercase === true && lowercase === false && numeric === false && special === true) {
-    charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
-  } else if (uppercase === false && lowercase === false && numeric === true && special === false) {
-    charString = "1234567890";
-  } else if (uppercase === false && lowercase === false && numeric === true && special === true) {
-    charString = "1234567890!@#$%^&*";
-  } else if (uppercase === false && lowercase === false && numeric === false && special === true) {
-    charString = "!@#$%^&*";
-  } else if (uppercase === true && lowercase === true && numeric === true && special === true) {
-    charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
-  } 
+if (lowercase === true) {
+  chars = chars + "abcdefghijklmnopqrstuvwxyz";
+} else {
+  chars = chars;
+}
+
+if (uppercase === true) {
+  chars = chars + "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+} else {
+  chars = chars;
+}
+
+if (numeric === true) {
+  chars = chars + "1234567890";
+} else {
+  chars = chars;
+}
+
+if (special === true) {
+  chars = chars + "!@#$%^&*";
+} else {
+  chars = chars;
+}
+
+console.log(chars);
+
+  // if (uppercase === false && lowercase === true && numeric === false && special === false) {
+  //   charString = "abcdefghijklmnopqrstuvwxyz";
+  // } else if (uppercase === true && lowercase === true && numeric === false && special === false) {
+  //   charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // } else if (uppercase === true && lowercase === true && numeric === true && special === false) {
+  //   charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  // } else if (uppercase === true && lowercase === true && numeric === false && special === true) {
+  //   charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
+  // } else if (uppercase === false && lowercase === true && numeric === true && special === false) {
+  //   charString = "abcdefghijklmnopqrstuvwxyz1234567890";
+  // } else if (uppercase === false && lowercase === true && numeric === false && special === true) {
+  //   charString = "abcdefghijklmnopqrstuvwxyz!@#$%^&*";
+  // } else if (uppercase === false && lowercase === true && numeric === true && special === true) {
+  //   charString = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+  // } else if (uppercase === true && lowercase === false && numeric === false && special === false) {
+  //   charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // } else if (uppercase === true && lowercase === false && numeric === true && special === false) {
+  //   charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+  // } else if (uppercase === true && lowercase === false && numeric === true && special === true) {
+  //   charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+  // } else if (uppercase === true && lowercase === false && numeric === false && special === true) {
+  //   charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*";
+  // } else if (uppercase === false && lowercase === false && numeric === true && special === false) {
+  //   charString = "1234567890";
+  // } else if (uppercase === false && lowercase === false && numeric === true && special === true) {
+  //   charString = "1234567890!@#$%^&*";
+  // } else if (uppercase === false && lowercase === false && numeric === false && special === true) {
+  //   charString = "!@#$%^&*";
+  // } else if (uppercase === true && lowercase === true && numeric === true && special === true) {
+  //   charString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*";
+  // }
+  
 
   for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * charString.length -1);
-    password += charString.substring(randomNumber, randomNumber +1);
+    var randomNumber = Math.floor(Math.random() * charString.length - 1);
+    password += chars.substring(randomNumber, randomNumber +1);
   }
   
-  console.log(password);
+// for (var i = 0; i<= passwordLength; i++) {
+//   var randomNumber = Math.floor(Math.random() * charString.length )
+// }
+
 
   document.getElementById("password").value = password;
-  
-
-
- 
-
-
 
   passwordText = password;
 
